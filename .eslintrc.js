f7f82debadf5946145b5bf69153e937d0d5e0560
NikNikich <nikolay.andreev@omega-r.com>
2020-12-03 23:46:47 +0200

    done edit esLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,49 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
   env: {
     node: true,
     jest: true,
   },
-  extends: [ 'airbnb-typescript/base' ],
-  parserOptions: {
-    project: 'tsconfig.json',
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
+    "no-prototype-builtins": "off",
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    "import/prefer-default-export": "off",
+    // single export in nest js use modules
+    "import/no-default-export": "off",
+    //example is dependency in entity postgres(many-to-one e.t.c)
+    "import/no-cycle": "off",
+    // in loop use await
+    "no-await-in-loop": "off",
+    // Use function hoisting to improve code readability
+    "no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true },
+    ],
+    // Makes no sense to allow type inferrence for expression parameters, but require typing the response
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      { allowExpressions: true, allowTypedFunctionExpressions: true },
+    ],
+    //familiar language feature
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+    // Common abbreviations are known and readable
+    "unicorn/prevent-abbreviations": "off",
   },
 };
