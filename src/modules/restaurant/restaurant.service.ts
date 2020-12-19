142a628a1781b4c13240b031d1448968b70c8bd6
NikNikich <nikolay.andreev@omega-r.com>
2020-12-19 16:35:25 +0300

    add restaurant selected and reformat code

--- src/modules/restaurant/restaurant.service.ts
+++ src/modules/restaurant/restaurant.service.ts
@@ -3,15 +3,16 @@ import { RestaurantRepository } from '@modules/restaurant/repository/restaurant.
 import { RestaurantEntity } from '@modules/restaurant/entity/restaurant.entity';
 import { MenuItemService } from '@modules/menu-item/menu-item.service';
 import { RestaurantMenuResponseDto } from '@modules/restaurant/dto/response/restaurant-menu.response.dto';
-import { CommentService } from '@modules/comment/comment.service';
 import { RestaurantCommentsResponseDto } from '@modules/restaurant/dto/response/restaurant-comments.response.dto';
+import { UserRepository } from '@modules/user/repository/user.repository';
+import { UserEntity } from '@modules/user/entity/user.entity';
 
 @Injectable()
 export class RestaurantService {
   constructor(
+    private readonly userRepository: UserRepository,
     private readonly restaurantRepository: RestaurantRepository,
     private readonly menuItemService: MenuItemService,
-    private readonly commentService: CommentService,
   ) {}
 
   async findAll(): Promise<RestaurantEntity[]> {
@@ -31,16 +32,37 @@ export class RestaurantService {
   async getRestaurantWithComments(
     restaurantId: number,
   ): Promise<RestaurantCommentsResponseDto> {
-    const restaurant = await this.getRestaurant(restaurantId);
-    const comments = await this.commentService.getRestaurantComments(
+    return this.restaurantRepository.getByIdWithCommentsOrReject(
       restaurantId,
     );
-    return { ...restaurant, comments };
+  }
+
+  async setRestaurantSelected(
+    restaurantId: number,
+    userId: number,
+  ): Promise<RestaurantEntity> {
+    const restaurant = await this.restaurantRepository.getByIdWithSelectedUserOrReject(
+      restaurantId,
+    );
+    if (this.isRestaurantSelected(restaurant.selectedUser, userId)) {
+      return restaurant;
+    }
+    const user = await this.userRepository.findByIdOrReject(userId);
+    restaurant.selectedUser.push(user);
+    return this.restaurantRepository.save(restaurant);
   }
 
   async getRestaurant(
     restaurantId: number,
   ): Promise<RestaurantEntity> {
-    return this.restaurantRepository.findByIdOrReject(restaurantId);
+    return this.restaurantRepository.getByIdOrReject(restaurantId);
+  }
+
+  private isRestaurantSelected(
+    selectedUser: UserEntity[],
+    userId: number,
+  ): boolean {
+    const findUser = selectedUser.find((user) => user.id === userId);
+    return !!findUser;
   }
 }
